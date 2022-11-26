104f33f1c1bb659df2b807c9279ef39efeac207a
levimake <levimake10@gmail.com>
2022-11-26 23:28:13 +0530

    day-1-full

--- day-1-5.py
+++ day-1-5.py
@@ -1,4 +1,4 @@
 print("Hello World!")
 city = input("Name the city you grew up in?\n")
 pet = input("Name of your pet?\n")
-print("Your band name could be",city+pet)
+print("Your band name could be " + city + " " + pet)
